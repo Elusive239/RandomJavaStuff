12f603ce0491f16a04293459636b063f5cabdc08
Elusive239 <56003608+Elusive239@users.noreply.github.com>
2020-04-17 02:41:45 -0400

    Tic-Tac-Toe Thing
    A thing I made Because I had the time

---
+++ Lab-12-McGuff-Two-Electric-Boogaloo/src/com/company/Main.java
@@ -0,0 +1,14 @@
+/**
+ * It's tic tac toe. Shouldn't really need an explanation.
+ */
+package com.company;
+
+/**
+ * @author Thomas L. McGuff
+ * ive got time to spare so this is just what I came up with for fun
+ */
+public class Main {
+    public static void main(String[] args) {
+        GameBoard gb = new GameBoard();//creates a new GameBoard object
+    }
+}
